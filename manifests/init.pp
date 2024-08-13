7891153e867a7ece530fe39bb033b9f632995f21
Maxime Boissonneault <maxime.boissonneault@calculquebec.ca>
2024-08-13 11:36:27 -0400

    create user tutor

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,3 +1,12 @@
 class tutor {
   ensure_resource('class', 'tutor::base', { 'install_docker' => true })
+
+	user { 'tutor':
+	  ensure  => 'present',
+		gid     => 'tutor',
+		groups  => 'docker',
+		require => [
+		  Package["docker-ce"]
+		]
+	}
 }
