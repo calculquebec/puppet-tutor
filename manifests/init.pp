8f9a9d4487445c4b57f89e5919baa709a8c34860
Maxime Boissonneault <maxime.boissonneault@calculquebec.ca>
2024-08-13 11:37:59 -0400

    create group tutor

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,12 +1,16 @@
 class tutor {
   ensure_resource('class', 'tutor::base', { 'install_docker' => true })
 
+  group { 'tutor': 
+	  ensure => 'present',
+	}
 	user { 'tutor':
 	  ensure  => 'present',
 		gid     => 'tutor',
 		groups  => 'docker',
 		require => [
-		  Package["docker-ce"]
+		  Package["docker-ce"],
+			Group['tutor']
 		]
 	}
 }
