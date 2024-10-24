99d95d9796edd33f1656457573614ed2c4744e1e
Maxime Boissonneault <maxime.boissonneault@calculquebec.ca>
2024-10-24 13:54:46 -0400

    define a tutor::plugin type and use it for plugins

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,7 +1,65 @@
+define tutor::plugin (
+  String $tutor_user = 'tutor',
+  String $tutor_plugins_dir = "/${tutor_user}/.local/share/tutor-plugins",
+  String $content = '',
+  String $image = '',
+  Boolean $build_image_on_change = false,
+) {
+
+  if $content != '' {
+    file { "${tutor_plugins_dir}/${title}.py":
+      ensure  => 'present',
+      owner   => $tutor_user,
+      group   => $tutor_user,
+      require => File[$tutor_plugins_dir],
+      content => $content,
+      notify  => ($image == '' ? {
+                     true => Exec["tutor_plugins_enable_${title}"],
+                     false => Exec["tutor_plugins_enable_${title}", "tutor_images_build_${image}"],
+                 }),
+    }
+  }
+
+  exec { "tutor_plugins_enable_${title}":
+    command     => "tutor plugins enable ${title}",
+    unless      => "tutor plugins list | grep -w ${title} | grep -w enabled",
+    user        => $tutor_user,
+    path        => ['/usr/bin', '/usr/local/bin'],
+    notify      => ($image == '' ? {
+                     true  => Exec['tutor_config_save'],
+                     false => Exec['tutor_config_save', "tutor_images_build_${image}"],
+                   }),
+    require     => $require,
+  }
+
+  if $image != '' {
+    if $build_image_on_change {
+      exec { "tutor_images_build_${image}":
+        command     => "tutor images build ${image}",
+        user        => $tutor_user,
+        refreshonly => true,
+        path        => ['/usr/bin', '/usr/local/bin'],
+        timeout     => 1800,
+        notify      => Exec['tutor_local_reboot'],
+      }
+    }
+    else {
+      exec { "tutor_images_build_${image}":
+        command     => "tutor images build ${image}",
+        unless      => "docker images | grep -w ${image}",
+        user        => $tutor_user,
+        refreshonly => true,
+        path        => ['/usr/bin', '/usr/local/bin'],
+        timeout     => 1800,
+      }
+    }
+  }
+}
 class tutor (
   String $version = '18.1.3',
   Array[Tuple[String, String]] $config,
   Array[Tuple[String, String]] $env_patches = [],
+  String $brand_theme_url = '',
   String $admin_password,
   String $admin_email,
 ) {
@@ -51,37 +109,31 @@ class tutor (
   hooks.Filters.ENV_PATCHES.add_item(("<%= $tuple[0] %>", "<%= $tuple[1] %>"))
   <% } %>
   |END
-  file { $puppet_tutor_py:
-    ensure  => 'present',
-    owner   => $tutor_user,
-    group   => $tutor_user,
-    require => File[$tutor_plugins_dir],
-    content => inline_epp($puppet_tutor_py_template),
-    notify  => Exec['tutor_config_save'],
+  tutor::plugin { 'puppet_tutor':
+    tutor_user        => $tutor_user,
+    tutor_plugins_dir => $tutor_plugins_dir,
+    content           => inline_epp($puppet_tutor_py_template),
   }
 
-  exec { 'tutor_plugins_enable_puppet_tutor':
-    command     => 'tutor plugins enable puppet_tutor',
-    unless      => 'tutor plugins list | grep puppet_tutor | grep enabled',
-    user        => $tutor_user,
-    path        => ['/usr/bin', '/usr/local/bin'],
-    notify      => Exec['tutor_config_save'],
+  tutor::plugin { 'backup':
+    tutor_user => $tutor_user,
+    image      => 'backup',
+    require    => Package['tutor-contrib-backup'],
   }
 
-  exec { 'tutor_plugins_enable_backup':
-    command     => 'tutor plugins enable backup',
-    unless      => 'tutor plugins list | grep backup | grep enabled',
-    user        => $tutor_user,
-    path        => ['/usr/bin', '/usr/local/bin'],
-    notify      => Exec['tutor_config_save', 'tutor_backup_build_image'],
-  }
-
-  exec { 'tutor_backup_build_image':
-    command     => 'tutor images build backup',
-    unless      => 'docker images | grep backup',
-    user        => $tutor_user,
-    refreshonly => true,
-    path        => ['/usr/bin', '/usr/local/bin'],
+  if $brand_theme_url != '' {
+    $brand_theme_patch = @("END")
+    from tutor import hooks
+    hooks.Filters.ENV_PATCHES.add_item(("mfe-dockerfile-post-npm-install-authn", """
+    RUN npm install '@edx/brand@${brand_theme_url}'
+    """))
+    |END
+    tutor::plugin { 'brand_theme':
+      tutor_user            => $tutor_user,
+      image                 => 'mfe',
+      build_image_on_change => true,
+      content               => $brand_theme_patch,
+    }
   }
 
   exec { 'tutor_config_save':
@@ -124,6 +176,14 @@ class tutor (
     unless  => "tutor local status | grep overhangio/openedx | grep tcp",
     path    => ['/usr/bin', '/usr/local/bin'],
     require => Exec['tutor_local_do_init'],
+  }
+
+  exec { 'tutor_local_reboot':
+    command     => 'tutor local reboot --detach',
+    user        => $tutor_user,
+    path        => ['/usr/bin', '/usr/local/bin'],
+    require     => Exec['tutor_local_do_init'],
+    refreshonly => true,
   }
 
   exec { 'tutor_local_exec_lms_reload-uwsgi':
